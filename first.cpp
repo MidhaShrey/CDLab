66f836c126011c57721d6df37b89b62957aa4134
Shrey Midha <midhashrey@gmail.com>
2020-10-06 17:25:31 +0530

    06-10-2020

--- first.cpp
+++ first.cpp
@@ -1,117 +1,104 @@
-#include <Iostream>
-#include <stdio.h>
-#include <conio.h>
-#include <string.h>
-#include <stdlib.h>
-
+#include<Iostream>
+#include<ctype.h>
 using namespace std;
-typedef struct treenode
-{
-    char info;
-    treenode *left;
-    treenode *right;
-}node;
-
-
-    node * proc_e(char input[],int &ssm);
-    node * proc_t(char input[],int &ssm);
-    node * proc_v(char input[],int &ssm);
-    void traversal(node *temp);
-
+void FIRST(char[],char );
+void addToResultSet(char[],char);
+int numOfProductions;
+char productionSet[10][10];
 int main()
 {
-    char input[20];
-    int ssm=0;
-    node *root;
-
-    cout<<endl<<"ENTER THE STRING TO BE PARSED:";
-    cin>>input;
-
-    root = proc_e(input,ssm);
-    cout<<endl<<"CONTENTS OF THE PARSE TREE:";
-    traversal(root);
-
-
-    getch();
+    int i;
+    char choice; 
+    char c;
+    char result[20];
+    printf("How many number of productions ? :");
+    scanf(" %d",&numOfProductions);
+    for(i=0;i<numOfProductions;i++)//read production string eg: E=E+T
+    {
+        printf("Enter productions Number %d : ",i+1);
+        scanf(" %s",productionSet[i]);
+    }
+    do
+    {
+        printf("\n Find the FIRST of  :");
+        scanf(" %c",&c);
+        FIRST(result,c); //Compute FIRST; Get Answer in 'result' array
+        printf("\n FIRST(%c)= { ",c);
+        for(i=0;result[i]!='\0';i++)
+        printf(" %c ",result[i]);       //Display result
+        printf("}\n");
+         printf("press 'y' to continue : ");
+        scanf(" %c",&choice);
+    }
+    while(choice=='y'||choice =='Y');
     return 0;
 }
-
-
-void traversal(node *temp)
+/*
+ *Function FIRST:
+ *Compute the elements in FIRST(c) and write them
+ *in Result Array.
+ */
+void FIRST(char* Result,char c)
 {
-
-
-   if(temp != NULL)
-   {
-    traversal(temp->left);
-    cout<<temp->info;
-    traversal(temp->right);
+    int i,j,k;
+    char subResult[20];
+    int foundEpsilon;
+    subResult[0]='\0';
+    Result[0]='\0';
+    //If X is terminal, FIRST(X) = {X}.
+    if(!(isupper(c)))
+    {
+        addToResultSet(Result,c);
+               return ;
+    }
+    //If X is non terminal
+    //Read each production
+    for(i=0;i<numOfProductions;i++)
+    {
+//Find production with X as LHS
+        if(productionSet[i][0]==c)
+        {
+//If X → ε is a production, then add ε to FIRST(X).
+ if(productionSet[i][2]=='$') addToResultSet(Result,'$');
+            //If X is a non-terminal, and X → Y1 Y2 … Yk
+            //is a production, then add a to FIRST(X)
+            //if for some i, a is in FIRST(Yi),
+            //and ε is in all of FIRST(Y1), …, FIRST(Yi-1).
+      else
+            {
+                j=2;
+                while(productionSet[i][j]!='\0')
+                {
+                foundEpsilon=0;
+                FIRST(subResult,productionSet[i][j]);
+                for(k=0;subResult[k]!='\0';k++)
+                    addToResultSet(Result,subResult[k]);
+                 for(k=0;subResult[k]!='\0';k++)
+                     if(subResult[k]=='$')
+                     {
+                         foundEpsilon=1;
+                         break;
+                     }
+                 //No ε found, no need to check next element
+                 if(!foundEpsilon)
+                     break;
+                 j++;
+                }
+            }
     }
 }
-
-node * proc_e(char input[],int &ssm)
-{
-    node *a,*b;
-
-    a=proc_t(input,ssm);
-
-    while(input[ssm]=='+')
-    {
-        ssm++;
-        b=proc_t(input,ssm);
-
-        node *temp;
-        temp=new node; 
-        temp->info='+';
-        temp->left=a;
-        temp->right=b;
-
-        a=temp;
-
-    }
-
-    return a;
+    return ;
 }
-
-node * proc_t(char input[],int &ssm)
+/* addToResultSet adds the computed
+ *element to result set. 
+ *This code avoids multiple inclusion of elements
+  */
+void addToResultSet(char Result[],char val)
 {
-    node *a,*b;
-
-    a=proc_v(input,ssm);
-    ssm = ssm + 1;
-
-    while(input[ssm] == '*')
-    {
-        ssm++;
-        b=proc_v(input,ssm);
-        node *temp;
-        temp=new node;//(node *)malloc(sizeof(node));
-        temp->info='*';
-        temp->left=a;
-        temp->right=b;
-        a=temp;
-        ssm = ssm +1;
-    }
-    return a;
-}
-
-node * proc_v(char input[],int &ssm)
-{
-    node *a;
-
-    if(input[ssm]=='I')
-    {
-        node *temp;
-        temp=new node; 
-        temp->info='I';
-        temp->left=NULL;
-        temp->right=NULL;
-        return temp;
-    }
-    else
-    {
-        cout<<endl<<"ERROR. INVALID SYMBOL "<<input[ssm];
-        getch();
-        exit(0);
-    }
+    int k;
+    for(k=0 ;Result[k]!='\0';k++)
+        if(Result[k]==val)
+            return;
+    Result[k]=val;
+    Result[k+1]='\0';
 }
